f5d626cb5e7d3c02861b50f5a1be0fc6510a57fa
Piotr Narewski <pyotr.narevsky@gmail.com>
2024-03-28 12:48:36 +0100

    before migration change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SampleDotNet.Models;
 using System.Diagnostics;
+using SampleDotNet.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SampleDotNet.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var users = _context.Users.ToList();
+            return View(users);
+
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+
+
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Queries/SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO Users (Nick, Garma, Email, Password)
+VALUES
+    ('Gerry', 'gerry.derry@gmail.com', '123210'),
+    ('Jerry', 'jerry.derry@gmail.com', '123211'),
+    ('Berry', 'berry.derry@gmail.com', '123212'),
+    ('Terry', 'terry.derry@gmail.com', '123213'),
+    ('Perry', 'perry.derry@gmail.com', '123214');
