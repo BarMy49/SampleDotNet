4f008158702c28ea331fbdb92677d885694d0b1a
fl4ko <95556731+fl4ko@users.noreply.github.com>
2024-06-03 18:37:15 +0200

    Revert "ddd"

--- Models/Guser.cs
+++ Models/Guser.cs
@@ -9,10 +9,6 @@ namespace SampleDotNet.Models
         public int Garma { get; set; }
         public virtual ICollection<Post>? Posts { get; set; }
         public virtual ICollection<Gommunity>? Gommunities { get; set; }
-
-        public virtual ICollection<Message>? SentMessages { get; set; }
-        public virtual ICollection<Message>? ReceivedMessages { get; set; }
-
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<Reaction> Reactions { get; set; }
     }
